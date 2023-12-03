885baaa3903cbbc66301c0a2108c8c4dd2dc555b
hu8813n <ath.aok20@gmail.com>
2023-12-03 20:24:40 +0530

    Skip the given character using recursion

---
+++ src/Recursion/Strings/SkipCharacter.java
@@ -0,0 +1,22 @@
+package Recursion.Strings;
+
+public class SkipCharacter {
+    //Skip character "a" using recusrsion..
+    static void skip(String p , String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+        if(ch == 'a'){
+            skip( p , up.substring(1));
+        }else{
+            skip( p+ch, up.substring(1));
+        }
+    }
+
+    public static void main(String[] args) {
+        skip("" , "bccadack");
+
+    }
+}
