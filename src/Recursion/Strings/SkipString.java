088662c642d94ae0aa0b3c4b0efd95addd55666c
hu8813n <ath.aok20@gmail.com>
2023-12-03 20:37:37 +0530

    Skip the given string using recursion

--- src/Recursion/Strings/SkipString.java
+++ src/Recursion/Strings/SkipString.java
@@ -2,21 +2,21 @@ package Recursion.Strings;
 
 public class SkipString {
     //skip the given string
-    static void skip(String p , String up){
+    static String skip(String up){
         if(up.isEmpty()){
-            System.out.println(p);
-            return;
+           // System.out.println(p);
+            return "";
         }
-        char ch = up.charAt(0);
+
         if(up.startsWith("apple")){
-            skip( p , up.substring(5));
+           return up.charAt(0) + skip( up.substring(5));
         }else{
-            skip( p+ch, up.substring(1));
+           return up.charAt(0) + skip( up.substring(1));
         }
     }
 
     public static void main(String[] args) {
-        skip("" , "bccappledack");
+        System.out.println( skip( "bccappledack"));
 
     }
 }
